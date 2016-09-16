0dbc0cb32f073af104b2a4be2dfd6ab26ec3438f
Joshua Walton <jwalton922@gmail.com>
2016-09-16 15:21:58 -0600

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fantasyFootballNetworkApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fantasyFootballNetworkApp
+ */
+angular.module('fantasyFootballNetworkApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fantasyFootballNetworkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fantasyFootballNetworkApp
+ */
+angular.module('fantasyFootballNetworkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

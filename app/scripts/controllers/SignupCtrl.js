9b4d0d02389cf8d8c2d1b615a49ab0bef74c6e98
Josh Walton <jwalton922@gmail.com>
2017-07-26 00:20:02 -0600

    add alerts to other site linkage, remove checkbox for email updates for now

--- app/scripts/controllers/SignupCtrl.js
+++ app/scripts/controllers/SignupCtrl.js
@@ -3,8 +3,25 @@
 angular.module('fantasyFootballNetworkApp')
         .controller('SignupCtrl', ['$scope', '$log', function ($scope, $log) {
                 $scope.email = "";
+                $scope.receiveUpdates = false;
                 $scope.getEncodedUserEmail = function () {
                     $log.log("Getting encoded email for user", $scope.user);
                     return encodeURIComponent($scope.email);
+                };
+                
+                $scope.linkESPN = function(){
+                    alert("This feature is coming soon. Thanks for your patience!");
+                };
+                
+                $scope.linkNFL = function(){
+                    alert("This feature is coming soon. Thanks for your patience!");
+                };
+                
+                $scope.linkCBS = function(){
+                    alert("This feature is coming soon. Thanks for your patience!");
+                };
+                
+                $scope.linkOther = function(){
+                    alert("This feature is coming soon. Thanks for your patience!");
                 };
             }]);

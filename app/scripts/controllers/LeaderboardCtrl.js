167e9186680ec17cdb2f46a740348a9563fa1141
Josh Walton <josh.walton@pearson.com>
2017-08-09 11:27:14 -0600

    leaderboard

--- app/scripts/controllers/LeaderboardCtrl.js
+++ app/scripts/controllers/LeaderboardCtrl.js
@@ -2,13 +2,20 @@
 
 angular.module('fantasyFootballNetworkApp')
         .controller('LeaderboardCtrl', ['$scope', '$log', 'FootballApi',function ($scope, $log, FootballApi) {
-               $scope.offset = 0;
-               $scope.limit = 20;
+               $scope.currentPage = 1;
+               $scope.pageSize = 10;
                $scope.users = [];
+               $scope.totalUsers = 0;
                $scope.queryLeaderboard = function(){
-                   FootballApi.getLeaderboard($scope.offset,$scope.limit).then(function(xhr){
+                   $log.log("Current page: "+$scope.currentPage);
+                   var offset = ($scope.currentPage-1)*$scope.pageSize;
+                   FootballApi.getLeaderboard(offset,$scope.pageSize).then(function(xhr){
                        $log.log("leaderboard data",xhr);
-                       $scope.users = xhr.data;
+                       $scope.users = xhr.data.leaderboard;
+                       $scope.totalUsers = xhr.data.totalUsers;                       
+                       for(var i = 0; i < $scope.users.length; i++){
+                           $scope.users[i].rank = ($scope.currentPage-1)*$scope.pageSize+i+1;
+                       }
                    });
                };
                $scope.queryLeaderboard();

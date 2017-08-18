133e47de968efbcafda97798ecb7d93fec2e04ef
Josh Walton <josh.walton@pearson.com>
2017-08-17 22:30:07 -0600

    show loading icon handle link espn ret data change

--- app/scripts/controllers/SignupCtrl.js
+++ app/scripts/controllers/SignupCtrl.js
@@ -5,6 +5,7 @@ angular.module('fantasyFootballNetworkApp')
                 $scope.email = "";
                 $scope.receiveUpdates = false;
                 $scope.showESPN = false;
+                $scope.loadingESPN = false;
                 $scope.getEncodedUserEmail = function () {
                     $log.log("Getting encoded email for user", $scope.user);
                     return encodeURIComponent($scope.email);
@@ -15,9 +16,11 @@ angular.module('fantasyFootballNetworkApp')
                 };
                 
                 $scope.submitESPNLink = function(){
+                    $scope.loadingESPN = true;
                     FootballApi.linkESPN($scope.espnUsername, $scope.espnPassword,$scope.email).then(function(xhr){
                         $log.log("Link ESPN Response: ",xhr);
-                        $scope.showESPN = false;
+                        window.href.location=xhr.data.profileUrl;
+                        
                     });
                 };
                 
